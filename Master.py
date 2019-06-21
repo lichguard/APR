d99a8bcebe0dd746198ac6894184182ac3cb2923
Dean <dean.sha@gmail.com>
2019-06-21 15:34:43 +0300

    read question from tsv file

--- Master.py
+++ Master.py
@@ -1,17 +1,20 @@
 from DocumentStore import *
-
+from QuestionStore import *
 path = ""
 
 
 def main():
     ds = DocumentManager()
+    qs = QuestionManager()
 
     ds.create(path + "document_passages.json")
 
     doc = ds.get_document_by_id(344)
     para = doc.get_paragraph_by_id(1)
-
     print(para.get_text())
+
+    qs.create(path + "dev.tsv")
+    print(str(qs.questions[0].question))
 
 
 if __name__ == '__main__':
---
+++ QuestionStore.py
@@ -0,0 +1,41 @@
+import csv
+
+
+class Question:
+    qid = None
+    question = None
+    document_id = None
+    document_name = None
+    relevant_passages = []
+
+    def __init__(self, qid, question, document_id=None, document_name=None, relevant_passages=None):
+        self.qid = qid
+        self.question = question
+        self.document_id = document_id
+        self.document_name = document_name
+        self.relevant_passages = relevant_passages
+
+    def __str__(self):
+        return str(self.qid) + "\t" + str(self.question) + "\t" + str(self.document_id) + "\t" + str(self.document_name) + "\t" + \
+               str(self.relevant_passages) + "\n"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class QuestionManager:
+    questions = []
+
+    def __init__(self):
+        pass
+
+    def create(self, file_path):
+        self.questions = []
+        with open(file_path) as fd:
+            rd = csv.reader(fd, delimiter="\t", quotechar='"')
+            # ignores header row
+            next(rd)
+            for row in rd:
+                self.questions.append(Question(row[0], row[1], row[2], row[3],
+                                           [x.strip() for x in str(row[4]).split(',')]))
+
