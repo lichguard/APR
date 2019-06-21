bf523f0a6bfd5783bbec3f016a9361c84c42357b
Gilad <nomberg7@gmail.com>
2019-06-21 15:42:27 +0300

    little changes

--- Master.py
+++ Master.py
@@ -7,13 +7,13 @@ def main():
     ds = DocumentManager()
     qs = QuestionManager()
 
-    ds.create(path + "document_passages.json")
+    ds.create(path + "data\\document_passages.json")
 
     doc = ds.get_document_by_id(344)
     para = doc.get_paragraph_by_id(1)
     print(para.get_text())
 
-    qs.create(path + "dev.tsv")
+    qs.create(path + "data\\dev.tsv")
     print(str(qs.questions[0].question))
 
 
